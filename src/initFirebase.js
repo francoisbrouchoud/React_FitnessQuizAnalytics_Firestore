a02e10942369d8dd4ee91c7b57e7d5bb755e272f
Elias <borrajo.elias@gmail.com>
2023-04-11 23:32:37 +0200

    Revert "login"
    This reverts commit ae5f72841d9061a5a7a967da8c01b7566adbf107.

--- src/initFirebase.js
+++ src/initFirebase.js
@@ -1,17 +1,12 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-// import "firebase/compat/database"  // Real-time database
-import "firebase/compat/firestore"    // Firestore
+// import "firebase/compat/database" // Real-time database
+// import "firebase/compat/firestore" // Firestore
 
 // Configure Firebase.
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://fitnesscheck-webapp-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fitnesscheck-webapp",
-  storageBucket: "fitnesscheck-webapp.appspot.com",
-  messagingSenderId: "71433959621",
-  appId: "1:71433959621:web:a77fbfac033de01ff458ac"
   // Other configuration options, such as the Realtime Database / Firestore details...
 };
 export default firebase.initializeApp(config);
