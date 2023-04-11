19b1b5ddde0ab8a5d83ac0bb19fe268883df699a
François Brouchoud <francois@brouchoud.ch>
2023-04-11 16:06:29 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -34,11 +34,7 @@ function App() {
             </div>
 
             <SurveyPartTwo />
-
-
         </div>
-
-
 
     );
 }
@@ -68,7 +64,6 @@ function SurveyPartTwo() {
 
     const Survey = ({ questions, onSubmit }) => {
         const [responses, setResponses] = useState({});
-        //const [points, setPoints] = useState({});
         const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
         const handleChange = (event) => {
@@ -121,74 +116,6 @@ function SurveyPartTwo() {
         );
     };
 
-
-
-    // État local qui stocke les réponses de l'utilisateur
-    // const [reponses, setReponses] = useState([{id: 'a', text: 'Roufdggfe'}, { id: 'b', text: 'Bleu' },
-    //     { id: 'c', text: 'Vert' },]);
-
-    // Tableau de questions et réponses
-    // const questions = [
-    //     {
-    //         id: 1,
-    //         question: 'Quelle est votre couleur préférée ?',
-    //         reponses: [
-    //             { id: 'a', text: 'Rouge', isChecked: false },
-    //             { id: 'b', text: 'Bleu', isChecked: false },
-    //             { id: 'c', text: 'Vert' , isChecked: false},
-    //         ],
-    //     },
-    //     {
-    //         id: 2,
-    //         question: 'Quel est votre animal préféré ?',
-    //         reponses: [
-    //             { id: 'a', text: 'Chien' , isChecked: false},
-    //             { id: 'b', text: 'Chat' , isChecked: false},
-    //             { id: 'c', text: 'Oiseau' , isChecked: false},
-    //         ],
-    //     },
-    //     {
-    //         id: 3,
-    //         question: 'Quel est votre fruit préféré ?',
-    //         reponses: [
-    //             { id: 'a', text: 'Pomme' , isChecked: false},
-    //             { id: 'b', text: 'Banane' , isChecked: false},
-    //             { id: 'c', text: 'Orange', isChecked: false },
-    //         ],
-    //     },
-    // ];
-
-    // État local qui stocke l'index de la question actuelle
-    // const [index, setIndex] = useState(0);
-
-    // Fonction qui est appelée lorsque l'utilisateur clique sur le bouton "suivant"
-    // const handleNext = () => {
-    //     // Vérifie que l'utilisateur a sélectionné une réponse avant de passer à la question suivante
-    //     if (reponses[index]) {
-    //         // Incrémente l'index pour passer à la question suivante
-    //         setIndex(index + 1);
-    //     }
-    // };
-
-    // Fonction qui est appelée lorsque l'utilisateur coche une réponse
-    // const handleReponseChange = (e) => {
-    //     // Copie l'état actuel des réponses
-    //     const newReponses = [...reponses];
-    //     // Ajoute ou met à jour la réponse de l'utilisateur pour la question actuelle
-    //     newReponses[index] = e.target.value;
-    //     // Met à jour l'état des réponses
-    //     setReponses(newReponses);
-    //
-    //
-    //
-    //     //TODO set du checkbox response
-    // };
-
-
-    // Récupère la question et les réponses de la question actuelle à partir du tableau de questions
-    //const [,question, options] = questions[index];
-
-
     const questions = [
         {
             questionText: "Quels dispositifs d'aide à la marche utilisez vous ? ",
@@ -218,10 +145,7 @@ function SurveyPartTwo() {
             choices: ["impossible", "1 minute", "5 minutes", "15 minutes", "30 minutes", "1 heure", "2 heures", "3 heures & plus"],
             points: [0, 3, 6, 9, 12, 15, 18, 21]
         }
-
-
     ];
-
 
     const handleSubmit = (points) => {
         // go to final page with spider graphs
@@ -230,43 +154,12 @@ function SurveyPartTwo() {
 
 
     return (
-        <div>
             <div className="App">
                 <h1>Quizz</h1>
                 <Survey questions={questions} onSubmit={handleSubmit} />
             </div>
-
-            {/*<h1>Questionnaire</h1>*/}
-            {/*<h2>Question {index + 1}</h2>*/}
-            {/*<p>{questions[index].question}</p>*/}
-            {/*{*/}
-            {/*    questions[index].reponses.map((r) =>*/}
-            {/*            <div key={questions[index].reponses.id}>*/}
-            {/*                <form>*/}
-            {/*                    <label>*/}
-            {/*                        <input*/}
-            {/*                        type="radio"*/}
-            {/*                        name="reponse"*/}
-            {/*                        value={r.id}*/}
-            {/*                       // checked={q[index] === q.reponses.id}*/}
-            {/*                        checked={r.isChecked}*/}
-            {/*                        onChange={handleReponseChange}*/}
-            {/*                    />*/}
-            {/*                        <span>{r.text}</span>*/}
-            {/*                    </label>*/}
-            {/*                </form>*/}
-            {/*            </div>*/}
-
-            {/*    )*/}
-            {/*}*/}
-            {/*<button onClick={handleNext}>Suivant</button>*/}
-        </div>
     );
 }
-
-//export default Questionnaire;
-
-
 
 function ChangeColor(){
     const[color, setColor] = useState('black');
