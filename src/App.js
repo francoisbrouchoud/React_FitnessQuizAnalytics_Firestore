e2ee3ea3a10371cf76974b683718edd0b26f39d3
François Brouchoud <francois@brouchoud.ch>
2023-04-11 19:54:48 +0200

    feat: all questions for part 2 of quizz

--- src/App.js
+++ src/App.js
@@ -139,6 +139,7 @@ function SurveyPartTwo() {
         );
     };
 
+    //TODO gérer avec Firestore mais la structure est là
     const questions = [
         {
             questionId: 1,
@@ -172,6 +173,71 @@ function SurveyPartTwo() {
             questionSecondaryText: "Marcher rapidement (une vitesse plus rapide que celle de vos proches, de vos amis ou des gens de votre âge)",
             choices: ["impossible", "1 minute", "5 minutes", "15 minutes", "30 minutes", "1 heure", "2 heures", "3 heures & plus"],
             points: [0, 3, 6, 9, 12, 15, 18, 21]
+        },
+        {
+            questionId: 6,
+            questionText: "Montée d'escaliers : Reportez le degré de difficulté physique qui décrit le mieux la difficulté que vous avez eu à monter des escaliers, sans vous arrêter pour vous reposer, au cours de la dernière semaine.:",
+            questionSecondaryText: "Monter 1 étage ? Degré de difficulté",
+            choices: ["Aucun", "Leger", "Moyen", "Important", "Infaisable"],
+            points: [4, 3, 2, 1, 0]
+        },
+        {
+            questionId: 7,
+            questionText: "Montée d'escaliers : Reportez le degré de difficulté physique qui décrit le mieux la difficulté que vous avez eu à monter des escaliers, sans vous arrêter pour vous reposer, au cours de la dernière semaine.:",
+            questionSecondaryText: "Monter 3 étage ? Degré de difficulté",
+            choices: ["Aucun", "Leger", "Moyen", "Important", "Infaisable"],
+            points: [9, 8, 7, 6, 5]
+        },
+        {
+            questionId: 8,
+            questionText: "Montée d'escaliers : Reportez le degré de difficulté physique qui décrit le mieux la difficulté que vous avez eu à monter des escaliers, sans vous arrêter pour vous reposer, au cours de la dernière semaine.:",
+            questionSecondaryText: "Monter 5 étages ? Degré de difficulté",
+            choices: ["Aucun", "Leger", "Moyen", "Important", "Infaisable"],
+            points: [14, 13, 12, 11, 10]
+        },
+        {
+            questionId: 9,
+            questionText: "Avez-vous peur de l'altitude (Peur du vide) ? ",
+            questionSecondaryText: "Explication: La peur du vide est une vraie phobie (phobie = peur) et n'est pas à confondre avec le vertige, qui est un phénomène physiologique). La peur du vide peut se déclencher à la simple pensée de se retrouver en hauteur.",
+            choices: ["Oui", "Non"],
+            points: [1, 0]
+        },
+        {
+            questionId: 10,
+            questionText: "Avez-vous peur de l'altitude (Peur du vide) ? ",
+            questionSecondaryText: "Explication: La peur du vide est une vraie phobie (phobie = peur) et n'est pas à confondre avec le vertige, qui est un phénomène physiologique). La peur du vide peut se déclencher à la simple pensée de se retrouver en hauteur.",
+            choices: ["Oui", "Non"],
+            points: [1, 0]
+        },
+        {
+            questionId: 11,
+            questionText: "Avez-vous des douleurs?",
+            choices: ["Douleurs dans la hanche", "Douleurs des genoux", "Douleurs aux pieds", "Douleurs de dos", "Douleurs ailleurs", "Pas de douleurs"],
+            points: [1, 1, 1, 1, 1, 0]
+        },
+        {
+            questionId: 12,
+            questionText: "Est-ce que vous avez peur de chuter?",
+            choices: ["Oui, j'ai peur de chuter", "Non je n'ai pas peur de chuter"],
+            points: [1, 0]
+        },
+        {
+            questionId: 13,
+            questionText: "Avez-vous chuté lors des 12 derniers mois?",
+            choices: ["Non", "Une fois", "Deux fois", "Plusieurs fois"],
+            points: [0, 1, 2, 3]
+        },
+        {
+            questionId: 14,
+            questionText: "Avez-vous parfois des vertiges?",
+            choices: ["Oui", "Non"],
+            points: [1, 0]
+        },
+        {
+            questionId: 15,
+            questionText: "Est-ce que vous vous sentez sûre de vous lorsque vous vous tenez debout sur une seule jambe sans vous tenir à quelque chose?",
+            choices: ["Non je ne me sens pas sûr-e", "Oui je me sens sûr-e", "Impossible de faire sans me tenir"],
+            points: [1, 2, 0]
         }
     ];
 
