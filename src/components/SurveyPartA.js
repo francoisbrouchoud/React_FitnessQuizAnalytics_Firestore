5443410a8f4e81c742a8e3c562a6f7164dea4d72
François Brouchoud <francois@brouchoud.ch>
2023-04-25 23:54:09 +0200

    refactor components in function SurveyPartA

--- src/components/SurveyPartA.js
+++ src/components/SurveyPartA.js
@@ -21,7 +21,146 @@ export default function SurveyPartA({setResults, onComplete}) {
         document.title = surveyTitle;
     }, [surveyTitle]);
 
-    const QuestionZone = ({questionId, questionText, choices, onChange, value,}) => (
+
+    return (
+        <>
+            <h1>{surveyTitle}</h1>
+            {isLoading ? (
+                <p>Questions en cours de chargement.</p>
+            ) : (
+            <Survey questions={questionsFromDBPartA} setResults={setResults} onComplete={onComplete} />
+            )}
+        </>
+    );
+
+}
+
+function Survey ({ questions, setResults, onComplete })  {
+    const [responses, setResponses] = useState({});
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [questionHistory, setQuestionHistory] = useState([]);
+
+    const handleChange = (event) => {
+        const questionId = event.target.name;
+        const { value } = event.target;
+        setResponses({ ...responses, [questionId]: { id: questionId, value } });
+    };
+
+    //Tentative d'affichage de message quand plus de question
+    const next = () => {
+        if (!responses[question.questionId]) {
+            alert("Sélectionner une réponse svp.");
+            return;
+        }
+
+        if (responses.hasOwnProperty(question.questionId)) {
+            const nextQuestionOrPoints = getNextQuestion(
+                question.questionId,
+                responses[question.questionId].value,
+                responses,
+                questions
+            );
+
+            if (typeof nextQuestionOrPoints === 'number') {
+                const pointsA = {
+                    id: 'AQst',
+                    points: nextQuestionOrPoints.toString(),
+                };
+                setResults(pointsA);
+                onComplete();
+
+
+            } else {
+                setQuestionHistory([...questionHistory, currentQuestionIndex])
+                setCurrentQuestionIndex(
+                    questions.findIndex((q) => q.questionId === nextQuestionOrPoints.questionId)
+                );
+            }
+        }
+    };
+
+    const back = () => {
+        if (questionHistory.length > 0) {
+            setCurrentQuestionIndex(questionHistory[questionHistory.length -1]);
+            setQuestionHistory(questionHistory.slice(0, -1));
+        }
+    };
+
+
+    const getNextQuestion = (currentQuestionId, response, responses, questions) =>{
+        switch (currentQuestionId) {
+            case "AQst01":
+                if (response === "Oui") {
+                    return questions.find((q) => q.questionId === "AQst02");
+                } else {
+                    return questions.find((q) => q.questionId === "AQst04");
+                }
+            case "AQst02":
+                if (response  === "Oui") {
+                    return questions.find((q) => q.questionId === "AQst03");
+                } else {
+                    return 4;
+                }
+            case "AQst03":
+                if (response  === "Oui") {
+                    return 6;
+                } else {
+                    return 5;
+                }
+            case "AQst04":
+                if (response === "Oui") {
+                    return 3;
+                } else {
+                    return questions.find((q) => q.questionId === "AQst05");
+                }
+            case "AQst05":
+                return questions.find((q) => q.questionId === "AQst06");
+            case "AQst06":
+                const knowsBenefits = responses["AQst05"]?.value === "Oui";
+                const knowsRisks = responses["AQst06"]?.value === "Oui";
+
+                if (knowsBenefits || knowsRisks) {
+                    return 2;
+                } else {
+                    return 1;
+                }
+            default:
+                return null;
+        }
+    };
+
+
+
+    const question = questions[currentQuestionIndex];
+
+    return (
+        <div>
+            <QuestionZone
+                questionId={question.questionId}
+                questionText={question.questionText}
+                choices={question.choices}
+                onChange={handleChange}
+                value={responses[question.questionId]?.value}
+            />
+            <div className="controls-btn">
+                {currentQuestionIndex > 0 ? (
+                    <button className="secondary-button" type="button" onClick={back} disabled={currentQuestionIndex === 0}>
+                        Précédent
+                    </button>
+                ) : (
+                    <div style={{flex: 1}} />
+                )
+                }
+                <button className="primary-button" type="button" onClick={next}>
+                    Suivant
+                </button>
+            </div>
+        </div>
+    );
+};
+
+function QuestionZone({questionId, questionText, choices, onChange, value}) {
+    return (
         <div className="questionZone">
             <div className="card card-title">
                 <h3>{questionText}</h3>
@@ -42,138 +181,6 @@ export default function SurveyPartA({setResults, onComplete}) {
             </div>
         </div>
     );
-
-    const Survey = ({ questions, setResults, onComplete }) => {
-        const [responses, setResponses] = useState({});
-        const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-        const [questionHistory, setQuestionHistory] = useState([]);
-
-        const handleChange = (event) => {
-            const questionId = event.target.name;
-            const { value } = event.target;
-            setResponses({ ...responses, [questionId]: { id: questionId, value } });
-        };
-
-        //Tentative d'affichage de message quand plus de question
-        const next = () => {
-            if (!responses[question.questionId]) {
-                alert("Sélectionner une réponse svp.");
-                return;
-            }
-
-            if (responses.hasOwnProperty(question.questionId)) {
-                const nextQuestionOrPoints = getNextQuestion(
-                    question.questionId,
-                    responses[question.questionId].value,
-                    responses
-                );
-
-                if (typeof nextQuestionOrPoints === 'number') {
-                    const pointsA = {
-                        id: 'AQst',
-                        points: nextQuestionOrPoints.toString(),
-                    };
-                    setResults(pointsA);
-                    onComplete();
-
-
-                } else {
-                    setQuestionHistory([...questionHistory, currentQuestionIndex])
-                    setCurrentQuestionIndex(
-                        questions.findIndex((q) => q.questionId === nextQuestionOrPoints.questionId)
-                    );
-                }
-            }
-        };
-
-        const back = () => {
-            if (questionHistory.length > 0) {
-                setCurrentQuestionIndex(questionHistory[questionHistory.length -1]);
-                setQuestionHistory(questionHistory.slice(0, -1));
-            }
-        };
-
-
-        const question = questions[currentQuestionIndex];
-
-        return (
-            <div>
-                <QuestionZone
-                    questionId={question.questionId}
-                    questionText={question.questionText}
-                    choices={question.choices}
-                    onChange={handleChange}
-                    value={responses[question.questionId]?.value}
-                />
-                <div className="controls-btn">
-                    {currentQuestionIndex > 0 ? (
-                        <button className="secondary-button" type="button" onClick={back} disabled={currentQuestionIndex === 0}>
-                            Précédent
-                        </button>
-                    ) : (
-                        <div style={{flex: 1}} />
-                        )
-                    }
-                    <button className="primary-button" type="button" onClick={next}>
-                        Suivant
-                    </button>
-                </div>
-            </div>
-        );
-    };
-
-    const getNextQuestion = (currentQuestionId, response, responses) => {
-        switch (currentQuestionId) {
-            case "AQst01":
-                if (response === "Oui") {
-                    return questionsFromDBPartA.find((q) => q.questionId === "AQst02");
-                } else {
-                    return questionsFromDBPartA.find((q) => q.questionId === "AQst04");
-                }
-            case "AQst02":
-                if (response  === "Oui") {
-                    return questionsFromDBPartA.find((q) => q.questionId === "AQst03");
-                } else {
-                    return 4;
-                }
-            case "AQst03":
-                if (response  === "Oui") {
-                    return 6;
-                } else {
-                    return 5;
-                }
-            case "AQst04":
-                if (response === "Oui") {
-                    return 3;
-                } else {
-                    return questionsFromDBPartA.find((q) => q.questionId === "AQst05");
-                }
-            case "AQst05":
-                return questionsFromDBPartA.find((q) => q.questionId === "AQst06");
-            case "AQst06":
-                const knowsBenefits = responses["AQst05"]?.value === "Oui";
-                const knowsRisks = responses["AQst06"]?.value === "Oui";
-
-                if (knowsBenefits || knowsRisks) {
-                    return 2;
-                } else {
-                    return 1;
-                }
-            default:
-                return null;
-        }
-    };
-
-
-    return (
-        <>
-            <h1>{surveyTitle}</h1>
-            {isLoading ? (
-                <p>Questions en cours de chargement.</p>
-            ) : (
-            <Survey questions={questionsFromDBPartA} setResults={setResults} onComplete={onComplete} />
-            )}
-        </>
-    );
-
 }
+
+
