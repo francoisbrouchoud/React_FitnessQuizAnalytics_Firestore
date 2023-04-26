d0fdea6190ecc0d48d500e14d316c9a807113155
François Brouchoud <francois@brouchoud.ch>
2023-04-26 16:44:47 +0200

    feat: useEffect to handle completion of each Survey

--- src/screens/Questionnaire.js
+++ src/screens/Questionnaire.js
@@ -10,6 +10,7 @@ import SetResultsToFirebase from "../components/SetResultsToFirebase";
  * A : physical activity evaluation
  * B : mobility evaluation
  * C : optional entry of a group leader and send of responses
+ * As soon as the result of a part is completed, move on to the next questionnaire
  * @returns {JSX.Element} - current survey A then B then C
  * @constructor
  */
@@ -22,30 +23,34 @@ export default function Questionnaire() {
     const [displaySurveyC, setDisplaySurveyC] = useState(false);
 
     const navigate = useNavigate();
+
     useEffect(() => {
-        if(resultsA != null && resultsB != null && resultsC != null){
-            console.log("Results to set in Firebase",  resultsA, resultsB, resultsC)
+        if (resultsA !== null) {
+            setDisplaySurveyA(false);
+            setDisplaySurveyB(true);
+        }
+    }, [resultsA]);
+
+    useEffect(() => {
+        if (resultsB !== null) {
+            setDisplaySurveyB(false);
+            setDisplaySurveyC(true);
+        }
+    }, [resultsB]);
+
+    useEffect(() => {
+        if (resultsC !== null) {
+            console.log("Results to set in Firebase", resultsA, resultsB, resultsC);
             SetResultsToFirebase(resultsA, resultsB, resultsC);
             navigate('/');
         }
-    }, [resultsA, resultsB, resultsC]);
+    }, [resultsC]);
 
-    const handleCompleteA = () => {
-        setDisplaySurveyA(false);
-        setDisplaySurveyB(true);
-        setDisplaySurveyC(false);
-    };
-
-    const handleCompleteB = () => {
-        setDisplaySurveyA(false);
-        setDisplaySurveyB(false);
-        setDisplaySurveyC(true);
-    };
 
       return (
         <>
-            {displaySurveyA && <SurveyPartA setResults={setResultsA} onComplete={handleCompleteA} />}
-            {displaySurveyB && <SurveyPartB setResults={setResultsB} onComplete={handleCompleteB} />}
+            {displaySurveyA && <SurveyPartA setResults={setResultsA} />}
+            {displaySurveyB && <SurveyPartB setResults={setResultsB} />}
             {displaySurveyC && <SurveyPartC setResults={setResultsC} />}
         </>
       );
